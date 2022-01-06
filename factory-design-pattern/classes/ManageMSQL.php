b98739ff9899767387f43d539ca5a764ab57d1f8
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-06 11:10:46 -0800

    factory design pattern

---
+++ factory-design-pattern/classes/ManageMSQL.php
@@ -0,0 +1,19 @@
+<?php
+   class ManageMSQL{
+       public function setHost($host){
+          // code here
+       }
+
+       public function setDB($db){
+        // code here
+       }
+
+       public function setUser($user){
+        // code here
+       }
+
+       public function setpass($pass){
+        // code here
+       }
+   }
+?>
